f83e906affa14d43e8b0fe2c03de920e9cf468c5
LOUARDI Abdeltif <loaurdi.abdeltif@yahoo.fr>
2011-05-07 19:22:18 +0100

    Category select

--- src/App/WebBundle/Controller/DefaultController.php
+++ src/App/WebBundle/Controller/DefaultController.php
@@ -17,9 +17,6 @@ class DefaultController extends WebBaseController {
     }
 
     private function searchByWord(&$params) {
-        /**
-         * Search by word
-         */
         $query_get = $this->get('request')->query;
         $q = $query_get->get('q');
         if (!empty($q)) {
@@ -27,7 +24,7 @@ class DefaultController extends WebBaseController {
             $params['where'] = "LOWER(a.title) like '%$q%'";
         }
     }
-    
+
     private function searchByCategory(&$params) {
         $category_id = $this->get('request')->get('category_id');
         $slug = $this->get('request')->get('slug');
