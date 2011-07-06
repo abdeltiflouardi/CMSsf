c945aa4a507f8d35074b009e15ff0e7f6131ab26
Abdeltif Louardi <abdeltif.louardi@interne.07zr.com>
2011-07-06 10:40:52 +0000

    TestUnit of routes:     home     Category     Sub_category     Slug

--- src/App/WebBundle/Tests/Controller/DefaultControllerTest.php
+++ src/App/WebBundle/Tests/Controller/DefaultControllerTest.php
@@ -7,15 +7,28 @@ class DefaultControllerTest extends WebTestCase
 {
     public function testIndex()
     {
-        $client = $this->createClient();
+        $client = static::createClient();
 
-        $crawler = $client->request('GET', '/php-symfony/1');
+        // Home
+        $crawler = $client->request('GET', '/');
         $this->assertTrue($client->getResponse()->isSuccessful());
         
-        // $crawler = $client->request('GET', '/2-symfony');
-        // $this->assertTrue($client->getResponse()->isSuccessful());
+        // Category
+        $crawler = $client->request('GET', '/web/2');
+        $link = $crawler->filter('a:contains("Web")')->eq(1)->link();
+        $crawler = $client->click($link);
+        $this->assertTrue($crawler->filter('div:contains("Created by")')->count() > 0);
         
-        // $crawler = $client->request('GET', '/3-ddd');
-        // $this->assertTrue($client->getResponse()->isSuccessful());        
+        // Sub-category
+        $crawler = $client->request('GET', '/web/2');
+        $link = $crawler->filter('a:contains("PHP")')->eq(1)->link();
+        $crawler = $client->click($link);
+        $this->assertTrue($crawler->filter('div:contains("Created by")')->count() > 0);
+        
+        // Slug
+        $crawler = $client->request('GET', '/php/5');
+        $link = $crawler->filter('a[href="/tag/1-php"]')->eq(1)->link();
+        $crawler = $client->click($link);
+        $this->assertTrue($crawler->filter('div:contains("Created by")')->count() > 0);        
     }
 }
