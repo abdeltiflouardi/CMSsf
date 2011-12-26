f210b766c977e915cb1ff13cf411d379feae2bba
ouardisoft <louardi.abdeltif@yahoo.fr>
2011-12-26 21:41:02 +0000

    Integrate SitemapBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -24,6 +24,7 @@ class AppKernel extends Kernel {
             new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
             new Knp\Bundle\MarkdownBundle\KnpMarkdownBundle(),
             new OS\RssBundle\OSRssBundle(),
+            new OS\SitemapBundle\OSSitemapBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
